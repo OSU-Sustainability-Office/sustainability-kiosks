48474f7572dc4c2b8c61290c377ad1b866637a3b
Jeff Huang <82061589+solderq35@users.noreply.github.com>
2024-04-16 16:30:29 -0700

    Back button refresh / deploy refresh automation (#11)
    * refresh on back
    
    * test deploy automation
    
    * publicpath smh
    
    * testing 30s
    
    * delay comment
    
    * add todo for font, fix env check
    
    * fix test build workflow
    
    * last cleanup todos?
    
    * KioWare testing
    
    * KioWare testing
    
    * Reset refreshinterval
    
    * Reset title
    
    * remove back button

--- src/App.vue
+++ src/App.vue
@@ -19,17 +19,72 @@
 export default {
   name: 'App',
   components: {},
-  async created () {},
+  async created () {
+    // For future reference on how back button doesn't clear cache (for MU kiosk)
+    // https://stackoverflow.com/a/75952012
+    // https://stackoverflow.com/questions/58652880/what-is-the-replacement-for-performance-navigation-type-in-angular
+
+    // turned off for local development for reasons explained in "watch: " section
+    if (process.env.NODE_ENV !== 'development') {
+      this.timer = setInterval(
+        this.fetchLastModified,
+        this.refreshInterval * 1000
+      ) // setInterval expects milliseconds
+    }
+  },
   data () {
     return {
       card: {
         name: 'foo'
-      }
+      },
+      timer: '',
+      url: process.env.VUE_APP_HOST_ADDRESS,
+      modifiedDateUnix: 0,
+      timeDiffUnix: 0,
+      refreshInterval: 600 // 10 minutes refresh interval. Time in seconds (lower for debug)
     }
   },
-  beforeDestroy () {},
-  methods: {},
-  watch: {}
+  methods: {
+    fetchLastModified () {
+      // https://stackoverflow.com/questions/36572540/vue-js-auto-reload-refresh-data-with-timer
+      // https://stackoverflow.com/a/74967740
+      fetch(this.url, { method: 'HEAD' }).then((r) => {
+        // string for when kiosk prod / staging was last modified, e.g. "Tue Apr 09 2024 14:29:24 GMT-0700 (Pacific Daylight Time)"
+        let modifiedDateString = new Date(r.headers.get('Last-Modified'))
+
+        // time in seconds for when kiosk prod / staging was last modified
+        this.modifiedDateUnix = Math.floor(modifiedDateString / 1000)
+
+        // time in seconds between last time kiosk prod / staging was modified and now
+        this.timeDiffUnix =
+          Math.floor(Date.now() / 1000) - this.modifiedDateUnix
+
+        // Log the modifiedDateUnix and timeDiffUnix here if needed for debug
+      })
+    },
+    cancelAutoUpdate () {
+      clearInterval(this.timer)
+    }
+  },
+  beforeDestroy () {
+    this.cancelAutoUpdate()
+  },
+  watch: {
+    // modifiedDateUnix is checked periodically as defined by this.refreshInterval
+    // if the modifiedDateUnix changes (not counting initial load), the page is reloaded
+    // https://v2.vuejs.org/v2/guide/computed.html#Watchers
+    modifiedDateUnix: function (newDate, oldDate) {
+      console.log(oldDate)
+      // NOTE: undefined last modified date (like on local dev) means the "oldDate" will always be 0
+      // It is recommended to test the refresh functionality on preview S3 deployment
+      if (oldDate !== 0) {
+        console.log('page reloading in 10 seconds')
+        setTimeout(() => {
+          window.location.reload()
+        }, 10000)
+      }
+    }
+  }
 }
 </script>
 
@@ -37,6 +92,13 @@ export default {
 @import url("https://fonts.googleapis.com/css?family=Open+Sans");
 @font-face {
   font-family: "StratumNo2";
+  /* TODO: Fix this sometime to dynamically change the Font URL prefix based on environment
+     In the meantime, use these prefixes (RESET TO PRODUCTION PREFIX BEFORE MERGE!):
+      - local / staging:
+        - "/fonts/"
+      - production:
+        - "/sustainability-kiosks/fonts/"
+  */
   src: url("/sustainability-kiosks/fonts/StratumNo2-Bold.woff2") format("woff2"),
     url("/sustainability-kiosks/fonts/StratumNo2-Bold.woff") format("woff"),
     url("/sustainability-kiosks/fonts/StratumNo2-Bold.ttf") format("truetype"),
