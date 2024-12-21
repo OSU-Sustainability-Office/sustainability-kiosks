046721dfba57df38046c16481c3bd1a479e0a380
Brandon Lee <brandovlee@Brandons-MacBook-Air.local>
2024-12-21 15:12:33 -0800

    Changed .js to .mjs extension ensuring ESM.

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath } from 'url'
 import path from 'path'
+import autoprefixer from 'autoprefixer'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
@@ -9,7 +14,7 @@ export default defineConfig(({ mode }) => {
     plugins: [vue()],
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, './src')
+        '@': path.resolve(__dirname, 'src')
       }
     },
     css: {
@@ -26,7 +31,7 @@ export default defineConfig(({ mode }) => {
       __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false
     },
     postcss: {
-      plugins: [require('autoprefixer')]
+      plugins: [autoprefixer()]
     }
   }
 })
