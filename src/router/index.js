ae34e00aa348ed9c38354a9a8b0a1a10bbff28b1
brandovlee <brandovlee@gmail.com>
2025-02-05 14:41:09 -0800

    Switch to hash mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import main from '@/components/main.vue'
 import sec from '@/components/sec.vue'
 import carousel from '@/components/carousel.vue'
@@ -24,7 +24,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.VITE_HOST_ADDRESS),
+  history: createWebHashHistory(import.meta.env.VITE_HOST_ADDRESS),
   routes
 })
 
